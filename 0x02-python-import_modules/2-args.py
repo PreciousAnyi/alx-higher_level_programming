d9f40aace59d5470490cb4f7e211211e773bb194
preciousanyi <jkpresh@gmail.com>
2024-01-04 13:53:39 -0800

    Added 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
         print("1 arguments:")
     else:
         print("{} arguments:".format(argc))
-    for i in argc:
+    for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
