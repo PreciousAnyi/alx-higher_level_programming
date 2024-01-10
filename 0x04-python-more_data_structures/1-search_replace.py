0b0e16cdb2dfbef4fd81704c7e719b22ea0d2b40
preciousanyi <jkpresh@gmail.com>
2024-01-10 20:02:45 +0100

    Added 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if i == search:
-            new_list.append(i)
-        new_list.append(i)
+            new_list.append(replace)
+        new_list.append(replace)
     return new_list
