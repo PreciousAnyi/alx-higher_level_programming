569a6089f3984cb6d2e471b7adec4587334abc9f
preciousanyi <jkpresh@gmail.com>
2024-01-11 01:04:40 +0100

    Added 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,9 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_value = max(a_dictionary.values())
-        best_key = [] 
+        best_key = []
         for key, value in a_dictionary.items():
             if value == max_value:
-                best_key.append(ke)y
+                best_key.append(key)
         return best_key[0]
     return None
