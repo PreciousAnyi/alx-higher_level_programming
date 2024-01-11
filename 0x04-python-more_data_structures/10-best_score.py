3c007f6629869f0b384c98e11c0c6fa4328902b8
preciousanyi <jkpresh@gmail.com>
2024-01-11 01:02:09 +0100

    Added 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_value = max(a_dictionary.values())
+        best_key = [] 
+        for key, value in a_dictionary.items():
+            if value == max_value:
+                best_key.append(ke)y
+        return best_key[0]
+    return None
