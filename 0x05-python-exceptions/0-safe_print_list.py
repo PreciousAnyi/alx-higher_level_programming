b2c368b1430c51a458956f37a424952595f58089
preciousanyi <jkpresh@gmail.com>
2024-01-22 20:20:56 +0100

    Added 0-safe_print_list.py, README.md

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = x
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i],end=""))
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
+
