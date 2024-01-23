02acf25958b5a4564c5c5d9e0692aef95bee2e3b
preciousanyi <jkpresh@gmail.com>
2024-01-23 21:10:27 +0100

    Added 0-square.py README.md

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+     Attributes:
+        No attributes are defined for an empty square.
+
+     Methods:
+        No methods are defined for an empty square.
+    """
+    pass
