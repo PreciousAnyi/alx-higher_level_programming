d6dd8624e3068be043665f988ae03ae2e5a945e1
preciousanyi <jkpresh@gmail.com>
2024-01-23 21:58:28 +0100

    Added 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,7 +17,7 @@ class Square:
         Parameters:
             size (int): The size of the square. Default is 0.
         """
-        if not isInstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
