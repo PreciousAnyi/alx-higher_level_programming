2af1ea09a83b660070904b8db83ab6646c8c9268
preciousanyi <jkpresh@gmail.com>
2024-01-29 20:07:45 +0100

    Added multiple files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        No attributes are defined in this class.
+
+    Methods:
+        No methods are defined in this class.
+    """
+    pass
