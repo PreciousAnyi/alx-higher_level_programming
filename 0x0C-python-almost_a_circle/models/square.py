cca87052b9c881855ce6c3c40a2da1f48cf626bc
preciousanyi <jkpresh@gmail.com>
2024-02-12 19:55:08 +0100

    Modified file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,30 @@ class Square(Rectangle):
         """Overloading __str__"""
         return f"[Square] ({self.id}) {self.x}/{self.y}" \
             f" - {self.width}"
+
+    @property
+    def size(self):
+        """Gets size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes to the square"""
+        if args:
+            attr = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args[:len(attr)]):
+                setattr(self, attr[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def __str__(self):
+        """Returns a string representation
+        of the square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y}"
+    f" - {self.size}"
