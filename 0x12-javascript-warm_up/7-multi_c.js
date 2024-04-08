c564c0fd51be8ee5f27a52915e0eb11b156c0352
preciousanyi <jkpresh@gmail.com>
2024-04-08 17:22:45 +0100

    Added 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+    console.log('Missing number of occurrences');
+} else if(firstArg > 0) {
+  for (let i = 0; i < firstArg; i++) {
+    console.log('C is fun');
+  }
+}
