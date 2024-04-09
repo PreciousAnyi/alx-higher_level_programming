690cd4b27966f2ff57c9bfee93a474d68152a367
preciousanyi <jkpresh@gmail.com>
2024-04-09 19:33:04 +0100

    Modified 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
       return Object.assign(this, {});
     } else {
       this.width = w;
