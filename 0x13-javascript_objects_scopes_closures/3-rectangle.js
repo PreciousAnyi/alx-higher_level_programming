28ec8e3216839a93d956cce423d2c3ca9ce3cfd0
preciousanyi <jkpresh@gmail.com>
2024-04-09 20:05:09 +0100

    Added 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return Object.assign(this, {});
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      const width = this.width;
+      const height = this.height;
+      for (let i = 0; i < height; i++) {
+        console.log('x'.repeat(width));
+      }
+    }
+  }
+}
+module.exports = Rectangle;
