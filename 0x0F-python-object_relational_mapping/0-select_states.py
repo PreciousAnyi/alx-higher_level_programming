6b68ca277e47739fa67aaf5a9e6244d623d47640
preciousanyi <jkpresh@gmail.com>
2024-04-14 13:31:19 -0700

    Added multiple files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Script to list all states from the hbtn_0e_0_usa database.
+
+This script connects to a MySQL database and retrieves all states
+from the states table, sorting them by ID in ascending order.
+
+Usage:
+    ./select_states.py <username> <password> <database>
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: Name of the database
+"""
+
+import sys
+import MySQLdb
+
+
+def get_states(username, password, db_name):
+    """
+    Retrieve and print all states from the database.
+
+    Args:
+        username (str): MySQL username
+        password (str): MySQL password
+        db_name (str): Name of the database
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                          passwd=password, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY ID ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    get_states(username, password, db_name)
